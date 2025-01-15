ced33357caa07b5154219cc9aba0c4bb511b7c71
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-01-15 10:40:58 +0100

    FEAT: converted question state to reducer

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,9 +1,23 @@
+import { useReducer } from "react";
+import { useState } from "react";
 
 
 export function Question() {
+
+
+  function questionReducer(question,action){
+        switch(action.type){
+          case "UPDATE_QUESTION":
+              return {...question,question:action.payload}
+          default:
+          return question
+        }
+  }
+
+
  
     // question data
-    const sampleQuestion = {
+    const [question,dispatch] = useReducer(questionReducer,{
         question: "What is the first thing you should do if you find yourself stranded on a deserted island?",
         options: [
           "Look for food",
@@ -13,21 +27,26 @@ export function Question() {
           "Explore the island"
         ],
         correctAnswer: "Find or build shelter"
-      }
+      })
 
-      const optionList = sampleQuestion.options.map(option => <li>{option}</li>)
+      const optionList = question.options.map(option => <li>{option}</li>)
 
     // way to answer the question
+
+    // show next
+    function updateQuestion(){
+      dispatch({type:"UPDATE_QUESTION", payload:"Why reducers"})
+    }
 
     // way to store the results
 
   return (
     <div>
-        <h1>{sampleQuestion.question}</h1>
+        <h1>{question.question}</h1>
         <ul>
             {optionList}
         </ul>
-      
+      <button onClick={updateQuestion}>Next</button>
     </div>
   );
 }
